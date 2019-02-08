6ea52293bb09e6e0b40bb20448a2ec99a20c8a01
malodmor <malodmor@gmail.com>
2019-02-08 23:22:06 +0100

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3c0358502fffc04ce854.js"
+    "url": "webpack-runtime-14493e149f6ae0d8dcc3.js"
   },
   {
     "url": "app-7b20912898f39a0fd6a1.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "abf84d70220e27f7fc43d800e9cce712"
+    "revision": "7cdc14bbb66038d544e3d1d1adff8b2b"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
